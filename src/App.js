c62c7abb8f5011c7917af6befb78a0d07f5dad10
Rebekah Amerson <becka_811@hotmail.com>
2018-09-17 15:28:53 -0500

    Basic mobile layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header id="header">Header</header>
+      <div id="map">Map</div>
+      <div id="filter-options">Filter</div>
+      <div id="list-view">List</div>
+      <footer id="footer">Footer</footer>
       </div>
     );
   }
