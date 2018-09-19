cdd020755e374798a7d1e36a45182b0778e8fa34
Rebekah Amerson <becka_811@hotmail.com>
2018-09-19 11:21:33 -0500

    Specific layout completed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header id="header">Header</header>
+      <header id="header">
+        <h1>My Hometown: Pittsburg, KS</h1>
+      </header>
       <div id="map">Map</div>
-      <div id="filter-options">Filter</div>
-      <div id="list-view">List</div>
-      <footer id="footer">Footer</footer>
+      <div id="search-options">
+        <form id="search-bar">
+          <input type="text" aria-label="search" placeholder="Search..."/>
+          <button aria-label="search button"><i className="fas fa-search"></i></button>
+        </form>
+      </div>
+      <div id="list-view">
+        <ul id="list">
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+          <li className="list-item">Some neat place!</li>
+        </ul>
+      </div>
+      <footer id="footer">Made using Google Maps and Foursquare API</footer>
       </div>
     );
   }
