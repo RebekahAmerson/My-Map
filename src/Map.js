fec1416bcd9ebc689719488d3873d4c6d4000144
Rebekah Amerson <becka_811@hotmail.com>
2018-09-24 09:21:16 -0500

    Map redo with npm to solve marker issue

--- src/Map.js
+++ src/Map.js
@@ -3,29 +3,27 @@ import React, { Component } from 'react';
 
 class Map extends Component {
 
-  componentDidMount() {
-     const screen = window.innerWidth;
-     let zoomSize;
-     if (screen >= 590) {
-       zoomSize = 12;
-     } else {
-       zoomSize = 11.5;
-     }
+componentDidMount() {
+  const screen = window.innerWidth;
+  let zoomSize;
+  if (screen >= 590) {
+   zoomSize = 12;
+  } else {
+   zoomSize = 11.5;
+  }
 
-     const script = document.createElement('script');
-     script.innerText = `mapboxgl.accessToken = 'pk.eyJ1IjoiYmVja2E4MTEiLCJhIjoiY2prY28ydWVtMDRtbzNxcnI3aGQzbWEyaCJ9.oFm0HaRTBybwDJyeRttpfQ';
-     var map = new mapboxgl.Map({
-       container: 'map',
-       style: 'mapbox://styles/mapbox/streets-v10',
-       center: [ -94.704709, 37.410496 ],
-       zoom: ${zoomSize}
-     });`;
-     document.body.appendChild(script);
-   }
+  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYmVja2E4MTEiLCJhIjoiY2prY28ydWVtMDRtbzNxcnI3aGQzbWEyaCJ9.oFm0HaRTBybwDJyeRttpfQ';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [ -94.704709, 37.410496 ],
+    zoom: zoomSize
+  });
+}
 
   render() {
-    const {locations} = this.props;
-
     return (
       <div id="map"></div>
     )
