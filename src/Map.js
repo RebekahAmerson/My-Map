7459d6b72dacbbde09f39680b7c3d0704e002901
Rebekah Amerson <becka_811@hotmail.com>
2018-09-24 12:01:40 -0500

    Markers work, hooray

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
+import mapboxgl from 'mapbox-gl';
 
 class Map extends Component {
+  state = {
+    mapReady: false
+  }
 
 componentDidMount() {
   const screen = window.innerWidth;
@@ -12,31 +15,30 @@ componentDidMount() {
    zoomSize = 11.5;
   }
 
-  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
   mapboxgl.accessToken = 'pk.eyJ1IjoiYmVja2E4MTEiLCJhIjoiY2prY28ydWVtMDRtbzNxcnI3aGQzbWEyaCJ9.oFm0HaRTBybwDJyeRttpfQ';
-  var map = new mapboxgl.Map({
+  this.map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
     center: [ -94.704709, 37.410496 ],
     zoom: zoomSize
   });
+  this.setState({mapReady: true});
 }
 
 makeMarker(locations) {
+  if (this.state.mapReady) {
   locations.forEach(location => {
     new mapboxgl.Marker()
     .setLngLat(location.coordinates)
-    .addTo(map);
-  })
+    .addTo(this.map);
+  })}
 }
 
   render() {
     const {locations} = this.props;
-
     return (
       <div id="map">
-        { this.makeMarker(locations) }
+      { this.makeMarker(locations) }
       </div>
     )
   }
